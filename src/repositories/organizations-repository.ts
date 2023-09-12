15e83ed3b5d73889b491930b808b970abd80de18
Raul Effting <rauleffting94@gmail.com>
2023-09-12 12:23:22 -0300

    feat: add findByEmail method to OrganizationsRepository

--- src/repositories/organizations-repository.ts
+++ src/repositories/organizations-repository.ts
@@ -2,4 +2,5 @@ import { Organization, Prisma } from '@prisma/client'
 
 export interface OrganizationsRepository {
   create(data: Prisma.OrganizationCreateInput): Promise<Organization>
+  findByEmail(email: string): Promise<Organization | null>
 }
