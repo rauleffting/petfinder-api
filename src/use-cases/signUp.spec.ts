347acf4dbdb344ed02d4c3fb4c0f6a4a2119da2a
Raul Effting <rauleffting94@gmail.com>
2023-09-12 12:14:44 -0300

    test: ensure SignUpUseCase can hash organizations password

--- src/use-cases/signUp.spec.ts
+++ src/use-cases/signUp.spec.ts
@@ -1,12 +1,18 @@
 import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations-repository'
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { SignUpUseCase } from './signUp'
+import { compare } from 'bcryptjs'
+
+let organizationsRepository: InMemoryOrganizationsRepository
+let sut: SignUpUseCase
 
 describe('Sign Up Use Case', () => {
-  it('should register an organization', async () => {
-    const organizationsRepository = new InMemoryOrganizationsRepository()
-    const sut = new SignUpUseCase(organizationsRepository)
+  beforeEach(() => {
+    organizationsRepository = new InMemoryOrganizationsRepository()
+    sut = new SignUpUseCase(organizationsRepository)
+  })
 
+  it('should register an organization', async () => {
     const { organization } = await sut.execute({
       name: 'Org Example',
       email: 'orgexample@example.com',
@@ -19,5 +25,22 @@ describe('Sign Up Use Case', () => {
     })
 
     expect(organization.id).toEqual(expect.any(String))
+  })
+
+  it('should hash organizations password upon registration', async () => {
+    const { organization } = await sut.execute({
+      name: 'Org Example',
+      email: 'orgexample@example.com',
+      password: '123456',
+      address: 'Example Street, 462',
+      city: 'Miami',
+      state: 'Florida',
+      postalCode: '33101',
+      phone: '58548400',
+    })
+
+    const isPasswordHashed = await compare('123456', organization.password_hash)
+
+    expect(isPasswordHashed).toBe(true)
   })
 })
