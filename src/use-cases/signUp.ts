18dd15b6e1103561752be42c8d1e09abd527b8fb
Raul Effting <rauleffting94@gmail.com>
2023-09-12 10:46:56 -0300

    feat: add SignUpUseCase

---
+++ src/use-cases/signUp.ts
@@ -0,0 +1,48 @@
+import { OrganizationsRepository } from '@/repositories/organizations-repository'
+import { Organization } from '@prisma/client'
+import { hash } from 'bcryptjs'
+
+interface SignUpUseCaseRequest {
+  name: string
+  email: string
+  password: string
+  address: string
+  city: string
+  state: string
+  postalCode: string
+  phone: string
+}
+
+interface SignUpUseCaseResponse {
+  organization: Organization
+}
+
+export class SignUpUseCase {
+  constructor(private organizationsRepository: OrganizationsRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    address,
+    city,
+    state,
+    postalCode,
+    phone,
+  }: SignUpUseCaseRequest): Promise<SignUpUseCaseResponse> {
+    const passwordHash = await hash(password, 6)
+
+    const organization = await this.organizationsRepository.create({
+      name,
+      email,
+      password_hash: passwordHash,
+      address,
+      city,
+      state,
+      postal_code: postalCode,
+      phone,
+    })
+
+    return { organization }
+  }
+}
