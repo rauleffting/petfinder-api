0b750ec4778932fed3519daa70b34efc4428a3b0
Raul Effting <rauleffting94@gmail.com>
2023-09-12 12:32:17 -0300

    test: ensure organization cannot register with same email twice

--- src/use-cases/signUp.spec.ts
+++ src/use-cases/signUp.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-mem
 import { beforeEach, describe, expect, it } from 'vitest'
 import { SignUpUseCase } from './signUp'
 import { compare } from 'bcryptjs'
+import { OrganizationAlreadyExistsError } from './errors/organization-already-exists-error'
 
 let organizationsRepository: InMemoryOrganizationsRepository
 let sut: SignUpUseCase
@@ -42,5 +43,33 @@ describe('Sign Up Use Case', () => {
     const isPasswordHashed = await compare('123456', organization.password_hash)
 
     expect(isPasswordHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const email = 'orgexample@example.com'
+
+    await sut.execute({
+      name: 'Org Example',
+      email,
+      password: '123456',
+      address: 'Example Street, 462',
+      city: 'Miami',
+      state: 'Florida',
+      postalCode: '33101',
+      phone: '58548400',
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'Org Example',
+        email,
+        password: '123456',
+        address: 'Example Street, 462',
+        city: 'Miami',
+        state: 'Florida',
+        postalCode: '33101',
+        phone: '58548400',
+      }),
+    ).rejects.toBeInstanceOf(OrganizationAlreadyExistsError)
   })
 })
