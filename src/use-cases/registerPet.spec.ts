565206217f6375651b5ffa10838878d1f5f862d2
Raul Effting <rauleffting94@gmail.com>
2023-09-13 16:19:40 -0300

    test: ensure pet can be registered by RegisterPetUseCase

---
+++ src/use-cases/registerPet.spec.ts
@@ -0,0 +1,46 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations-repository'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import { RegisterPetUseCase } from './registerPet'
+import { hash } from 'bcryptjs'
+import { Gender, Size, Type } from '@prisma/client'
+
+let organizationsRepository: InMemoryOrganizationsRepository
+let petsRepository: InMemoryPetsRepository
+let sut: RegisterPetUseCase
+
+describe('Register Pet Use Case', () => {
+  beforeEach(async () => {
+    petsRepository = new InMemoryPetsRepository()
+    organizationsRepository = new InMemoryOrganizationsRepository()
+    sut = new RegisterPetUseCase(petsRepository)
+
+    await organizationsRepository.create({
+      id: 'organization-01',
+      name: 'Org Example',
+      email: 'orgexample@example.com',
+      password_hash: String(hash('123456', 6)),
+      address: 'Example Street, 462',
+      city: 'Miami',
+      state: 'Florida',
+      postal_code: '33101',
+      phone: '58548400',
+    })
+  })
+
+  it('should register a pet', async () => {
+    const { pet } = await sut.execute({
+      name: 'Romeo',
+      description:
+        'He loves to play and eat. He also enjoys affection and going for walks in parks.',
+      animalType: Type.DOG,
+      gender: Gender.MALE,
+      size: Size.BIG,
+      age: '4',
+      breed: 'German Shepherd',
+      organizationId: 'organization-01',
+    })
+
+    expect(pet.id).toEqual(expect.any(Number))
+  })
+})
