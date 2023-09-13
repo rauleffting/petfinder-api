d1857c23b6d332d917f8dfd233fc51aa1c19f88c
Raul Effting <rauleffting94@gmail.com>
2023-09-13 17:01:37 -0300

    test: ensure organization can authenticate by SignInUseCase

---
+++ src/use-cases/sign-in.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { SignInUseCase } from './sign-in'
+import { hash } from 'bcryptjs'
+
+let organizationsRepository: InMemoryOrganizationsRepository
+let sut: SignInUseCase
+
+describe('Sign In Use Case', () => {
+  beforeEach(async () => {
+    organizationsRepository = new InMemoryOrganizationsRepository()
+    sut = new SignInUseCase(organizationsRepository)
+  })
+
+  it('should be able to sign in', async () => {
+    await organizationsRepository.create({
+      id: 'organization-01',
+      name: 'Org Example',
+      email: 'orgexample@example.com',
+      password_hash: String(hash('123456', 6)),
+      address: 'Example Street, 462',
+      city: 'Miami',
+      state: 'Florida',
+      postal_code: '33101',
+      phone: '58548400',
+    })
+
+    const { organization } = await sut.execute({
+      email: 'orgexample@example.com',
+      password: '123456',
+    })
+
+    expect(organization.id).toEqual(expect.any(String))
+  })
+})
