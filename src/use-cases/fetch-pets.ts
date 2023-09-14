5a5621e881d4f0dc64afaf8a3fcb503365737518
Raul Effting <rauleffting94@gmail.com>
2023-09-14 13:43:23 -0300

    feat: ensure user can filter pets using optional parameters

--- src/use-cases/fetch-pets.ts
+++ src/use-cases/fetch-pets.ts
@@ -1,9 +1,14 @@
 import { PetsRepository } from '@/repositories/pets-repository'
-import { Pet } from '@prisma/client'
+import { Gender, Pet, Size, Type } from '@prisma/client'
 
 interface FetchPetsUseCaseRequest {
   organizationsId: string[]
   page: number
+  animalType?: Type
+  gender?: Gender
+  size?: Size
+  age?: string
+  breed?: string
 }
 
 interface FetchPetsUseCaseResponse {
@@ -16,10 +21,20 @@ export class FetchPetsUseCase {
   async execute({
     organizationsId,
     page,
+    animalType,
+    gender,
+    size,
+    age,
+    breed,
   }: FetchPetsUseCaseRequest): Promise<FetchPetsUseCaseResponse> {
-    const pets = await this.petsRepository.findManyByOrganizationsId(
+    const pets = await this.petsRepository.findMany(
       organizationsId,
       page,
+      animalType,
+      gender,
+      size,
+      age,
+      breed,
     )
 
     return { pets }
