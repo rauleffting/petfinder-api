7727043afe8ec86e8cbffa9b51d82a7b0cdd0433
Raul Effting <rauleffting94@gmail.com>
2023-09-14 11:29:21 -0300

    refactor: remove organizations repository from register pet tests

--- src/use-cases/register-pet.spec.ts
+++ src/use-cases/register-pet.spec.ts
@@ -1,32 +1,16 @@
 import { beforeEach, describe, expect, it } from 'vitest'
-import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations-repository'
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
 import { RegisterPetUseCase } from './register-pet'
-import { hash } from 'bcryptjs'
 import { Gender, Size, Type } from '@prisma/client'
 import { PetWithoutOrganizationError } from './errors/pet-without-organization-error'
 
-let organizationsRepository: InMemoryOrganizationsRepository
 let petsRepository: InMemoryPetsRepository
 let sut: RegisterPetUseCase
 
 describe('Register Pet Use Case', () => {
   beforeEach(async () => {
     petsRepository = new InMemoryPetsRepository()
-    organizationsRepository = new InMemoryOrganizationsRepository()
     sut = new RegisterPetUseCase(petsRepository)
-
-    await organizationsRepository.create({
-      id: 'organization-01',
-      name: 'Org Example',
-      email: 'orgexample@example.com',
-      password_hash: await hash('123456', 6),
-      address: 'Example Street, 462',
-      city: 'Miami',
-      state: 'Florida',
-      postal_code: '33101',
-      phone: '58548400',
-    })
   })
 
   it('should register a pet', async () => {
