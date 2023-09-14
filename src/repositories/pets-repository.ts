ec6203ac38fb6cb646babecaac5f389b6e8c8f7d
Raul Effting <rauleffting94@gmail.com>
2023-09-14 13:03:17 -0300

    feat: add findManyByOrganizationsId method to PetsRepository

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -2,4 +2,8 @@ import { Pet, Prisma } from '@prisma/client'
 
 export interface PetsRepository {
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
+  findManyByOrganizationsId(
+    organizationsId: string[] | null,
+    page: number,
+  ): Promise<Pet[]>
 }
