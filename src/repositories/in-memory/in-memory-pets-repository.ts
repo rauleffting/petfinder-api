04ca27da139ba085932a8b7da8b63a05d3aebe4c
Raul Effting <rauleffting94@gmail.com>
2023-09-14 13:03:46 -0300

    chore: add findManyByOrganizationsId method to InMemoryPetsRepository

--- src/repositories/in-memory/in-memory-pets-repository.ts
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -5,6 +5,14 @@ import { randomInt } from 'crypto'
 export class InMemoryPetsRepository implements PetsRepository {
   public items: Pet[] = []
 
+  async findManyByOrganizationsId(organizationsId: string[], page: number) {
+    const pets = organizationsId.flatMap((organizationId) =>
+      this.items.filter((pet) => pet.organization_id === organizationId),
+    )
+
+    return pets.slice((page - 1) * 20, page * 20)
+  }
+
   async create(data: Prisma.PetUncheckedCreateInput) {
     const pet = {
       id: randomInt(1, 999999999),
