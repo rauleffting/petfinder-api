342efdd56e430563987d5c22906109ac5dd296ba
Raul Effting <rauleffting94@gmail.com>
2023-09-14 12:34:51 -0300

    chore: add findManyByCity method to InMemoryOrganizationsRepository

--- src/repositories/in-memory/in-memory-organizations-repository.ts
+++ src/repositories/in-memory/in-memory-organizations-repository.ts
@@ -1,11 +1,24 @@
 import { Organization, Prisma } from '@prisma/client'
 import { OrganizationsRepository } from '../organizations-repository'
 import { randomUUID } from 'crypto'
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
 
 export class InMemoryOrganizationsRepository
   implements OrganizationsRepository
 {
   public items: Organization[] = []
+
+  async findManyByCity(city: string) {
+    const organizations = this.items.filter(
+      (organization) => organization.city === city,
+    )
+
+    if (!organizations) {
+      throw new ResourceNotFoundError()
+    }
+
+    return organizations
+  }
 
   async findByEmail(email: string) {
     const organization = this.items.find((item) => item.email === email)
