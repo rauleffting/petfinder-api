3eede47ac39b48ff9efd85f02df75055da0de7a3
Raul Effting <rauleffting94@gmail.com>
2023-09-14 13:41:08 -0300

    chore: add findMany method on InMemoryPetsrepository and add a filter function into findMany

--- src/repositories/in-memory/in-memory-pets-repository.ts
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -1,16 +1,37 @@
-import { Pet, Prisma } from '@prisma/client'
+import { Gender, Pet, Prisma, Size, Type } from '@prisma/client'
 import { PetsRepository } from '../pets-repository'
 import { randomInt } from 'crypto'
 
 export class InMemoryPetsRepository implements PetsRepository {
   public items: Pet[] = []
 
-  async findManyByOrganizationsId(organizationsId: string[], page: number) {
-    const pets = organizationsId.flatMap((organizationId) =>
-      this.items.filter((pet) => pet.organization_id === organizationId),
-    )
+  async findMany(
+    organizationsId: string[],
+    page: number,
+    animalType?: Type,
+    gender?: Gender,
+    size?: Size,
+    age?: string,
+    breed?: string,
+  ) {
+    function filterFunction(pet: Pet) {
+      if (animalType && pet.animal_type !== animalType) return false
+      if (gender && pet.gender !== gender) return false
+      if (size && pet.size !== size) return false
+      if (age && pet.age !== age) return false
+      if (breed && pet.breed !== breed) return false
+      return true
+    }
 
-    return pets.slice((page - 1) * 20, page * 20)
+    const filteredPets = organizationsId
+      .flatMap((organizationId) =>
+        this.items.filter((pet) => pet.organization_id === organizationId),
+      )
+      .filter(filterFunction)
+
+    const pets = filteredPets.slice((page - 1) * 20, page * 20)
+
+    return pets
   }
 
   async create(data: Prisma.PetUncheckedCreateInput) {
