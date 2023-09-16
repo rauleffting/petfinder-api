a2fd2ce62f102695e8db2947555e05789d0c5bf5
Raul Effting <rauleffting94@gmail.com>
2023-09-16 12:16:25 -0300

    chore: add getPhotos method to InMemoryPhotoRepository

--- src/repositories/in-memory/in-memory-photo-repository.ts
+++ src/repositories/in-memory/in-memory-photo-repository.ts
@@ -5,6 +5,12 @@ import { PhotoRepository } from '../photo-repository'
 export class InMemoryPhotoRepository implements PhotoRepository {
   public items: Photo[] = []
 
+  async getPhotos(petId: number) {
+    const photos = this.items.filter((photo) => photo.pet_id === petId)
+
+    return photos
+  }
+
   async addPhoto(data: Prisma.PhotoUncheckedCreateInput) {
     const photo = {
       id: data.id ?? randomInt(1, 999999999),
