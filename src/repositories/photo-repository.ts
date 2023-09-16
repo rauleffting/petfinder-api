c3e07c2fd871c5b906dc14c0d94f82a2eb0501f1
Raul Effting <rauleffting94@gmail.com>
2023-09-16 12:15:36 -0300

    feat: add getPhotos method to PhotoRepository

--- src/repositories/photo-repository.ts
+++ src/repositories/photo-repository.ts
@@ -2,4 +2,5 @@ import { Photo, Prisma } from '@prisma/client'
 
 export interface PhotoRepository {
   addPhoto(data: Prisma.PhotoUncheckedCreateInput): Promise<Photo>
+  getPhotos(petId: number): Promise<Photo[]>
 }
