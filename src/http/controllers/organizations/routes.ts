c9ccb221b327a5d1ad81d417ae71eb6d313eef21
Raul Effting <rauleffting94@gmail.com>
2023-09-18 16:46:19 -0300

    feat: add refresh controller

--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { signUp } from './sign-up'
 import { signIn } from './sign-in'
+import { refresh } from './refresh'
 
 export async function organizationsRoutes(app: FastifyInstance) {
   app.post('/signup', signUp)
   app.post('/signin', signIn)
+  app.patch('/token/refresh', refresh)
 }
