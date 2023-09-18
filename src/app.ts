e6dab1c6143688388037422029f87fe6d3886dd6
Raul Effting <rauleffting94@gmail.com>
2023-09-18 16:10:17 -0300

    feat: register fastifyJwt and fastifyCookie and configure Error Handler on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,39 @@
+import { env } from './env'
+import { ZodError } from 'zod'
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { organizationsRoutes } from './http/controllers/organizations/routes'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
 app.register(organizationsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
