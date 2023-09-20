15f3e5c0e7b75d09a0b018f2849574ecefa60c79
Raul Effting <rauleffting94@gmail.com>
2023-09-20 15:18:21 -0300

    refactor: remove null return from findManyByCity on OrganizationsRepository

--- src/repositories/organizations-repository.ts
+++ src/repositories/organizations-repository.ts
@@ -3,5 +3,5 @@ import { Organization, Prisma } from '@prisma/client'
 export interface OrganizationsRepository {
   create(data: Prisma.OrganizationCreateInput): Promise<Organization>
   findByEmail(email: string): Promise<Organization | null>
-  findManyByCity(city: string): Promise<Organization[] | null>
+  findManyByCity(city: string): Promise<Organization[]>
 }
