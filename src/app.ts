ee5b22b49ba58a1d74433e2f46911deb11d252f7
Raul Effting <rauleffting94@gmail.com>
2023-09-20 10:24:44 -0300

    feat: register pet routes on app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
 import fastifyCookie from '@fastify/cookie'
 import { organizationsRoutes } from './http/controllers/organizations/routes'
+import { petsRoutes } from './http/controllers/pets/routes'
 
 export const app = fastify()
 
@@ -21,6 +22,7 @@ app.register(fastifyJwt, {
 app.register(fastifyCookie)
 
 app.register(organizationsRoutes)
+app.register(petsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
