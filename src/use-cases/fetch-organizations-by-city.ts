0fbce368258d93a69e2512e324a0c92cda9d4e03
Raul Effting <rauleffting94@gmail.com>
2023-09-20 15:17:18 -0300

    feat: add ResourceNotFoundError to FetchOrganizationsByCityUseCase

--- src/use-cases/fetch-organizations-by-city.ts
+++ src/use-cases/fetch-organizations-by-city.ts
@@ -1,12 +1,17 @@
 import { OrganizationsRepository } from '@/repositories/organizations-repository'
 import { Organization } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 export class FetchOrganizationsByCityUseCase {
   constructor(private organizationsRepository: OrganizationsRepository) {}
 
-  async execute(city: string): Promise<Organization[] | null> {
+  async execute(city: string): Promise<Organization[]> {
     const organizations =
       await this.organizationsRepository.findManyByCity(city)
+
+    if (!organizations) {
+      throw new ResourceNotFoundError()
+    }
 
     return organizations
   }
