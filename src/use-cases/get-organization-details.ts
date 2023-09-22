b49e2405b37e724011b9d7b11f5f281b2337a684
Raul Effting <rauleffting94@gmail.com>
2023-09-22 13:32:08 -0300

    feat: add FetchOrganizationByIdUseCase

---
+++ src/use-cases/get-organization-details.ts
@@ -0,0 +1,17 @@
+import { OrganizationsRepository } from '@/repositories/organizations-repository'
+import { Organization } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+export class FetchOrganizationByIdUseCase {
+  constructor(private organizationsRepository: OrganizationsRepository) {}
+
+  async execute(id: string): Promise<Organization> {
+    const organization = await this.organizationsRepository.findById(id)
+
+    if (!organization) {
+      throw new ResourceNotFoundError()
+    }
+
+    return organization
+  }
+}
